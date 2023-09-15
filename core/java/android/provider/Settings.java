8ecf60d13fbcf6f76ecfcce936f6ae93f424db3c
Dhina17 <dhinalogu@gmail.com>
2023-09-15 14:18:30 +0530

    GameManagerService: Set device_config property on behalf of GameSpace
    GTS don't allow any app to have the android.permission.WRITE_DEVICE_CONFIG permission
    i.e we can't modify device_config property from our game space.
    
    So write the property to our custom Settings.Secure.GAME_OVERLAY.
    Then observe the value changes and set the property to the
    device_config from here.
    
    Since GameManagerService is a part of system_server,
    GTS will be happy.
    
    Change-Id: I0ebbcd6188411a583fa53904e6153482f342a03e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11884,6 +11884,15 @@ public final class Settings {
                 "accessibility_floating_menu_migration_tooltip_prompt";
 
         /**
+         * Our GameSpace can't write to device_config directly [GTS]
+         * Use this as intermediate to pass device_config property
+         * from our GameSpace to com.android.server.app.GameManagerService
+         * so we can set the device_config property from there.
+         * @hide
+         */
+        public static final String GAME_OVERLAY = "game_overlay";
+
+        /**
          * Whether the Adaptive connectivity option is enabled.
          *
          * @hide
